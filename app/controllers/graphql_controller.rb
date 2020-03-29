1052b21994c57073b766ca4babb78d096073a688
Julien Camblan <julien.camblan@gmail.com>
2020-03-29 14:48:44 +0200

    fix: typo 🩹

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -52,7 +52,7 @@ class GraphqlController < ApplicationController
     logger.error error.backtrace.join("\n")
 
     render json: {
-      error: { message: error.message, backtrace: e.backtrace }, data: {}
+      error: { message: error.message, backtrace: error.backtrace }, data: {}
     }, status: 500
   end
 end
